c4bd6e8380b975d8935d72371478fc979bb20d2b
Nanibaka087 <128401880+Nanibaka087@users.noreply.github.com>
2024-11-01 19:30:42 +0700

    Add files via upload

---
+++ GameRoomForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Task1._4
+{
+    partial class GameRoomForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.pnlChat = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlSend = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlChat.SuspendLayout();
+            this.pnlSend.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.Location = new System.Drawing.Point(12, 12);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(511, 518);
+            this.pnlBoard.TabIndex = 0;
+            // 
+            // pnlChat
+            // 
+            this.pnlChat.Controls.Add(this.textBox1);
+            this.pnlChat.Location = new System.Drawing.Point(529, 12);
+            this.pnlChat.Name = "pnlChat";
+            this.pnlChat.Size = new System.Drawing.Size(337, 358);
+            this.pnlChat.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 352);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pnlSend
+            // 
+            this.pnlSend.Controls.Add(this.label1);
+            this.pnlSend.Controls.Add(this.button1);
+            this.pnlSend.Controls.Add(this.textBox3);
+            this.pnlSend.Controls.Add(this.textBox2);
+            this.pnlSend.Location = new System.Drawing.Point(529, 376);
+            this.pnlSend.Name = "pnlSend";
+            this.pnlSend.Size = new System.Drawing.Size(337, 154);
+            this.pnlSend.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(4, 32);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(234, 42);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gửi tin nhắn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Người chơi";
+            // 
+            // GameRoomForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 538);
+            this.Controls.Add(this.pnlSend);
+            this.Controls.Add(this.pnlChat);
+            this.Controls.Add(this.pnlBoard);
+            this.Name = "GameRoomForm";
+            this.Text = "GameRoomForm";
+            this.pnlChat.ResumeLayout(false);
+            this.pnlChat.PerformLayout();
+            this.pnlSend.ResumeLayout(false);
+            this.pnlSend.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Panel pnlChat;
+        private System.Windows.Forms.Panel pnlSend;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
